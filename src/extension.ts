ac67c490276450adeee2a25bf9a801689bd9b325
Iurii Shchekochikhin <iurii.shchekochikhin@broadcom.com>
2020-03-16 12:05:55 +0100

    Find files example command

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,19 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-samples-extension" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	let disposable = vscode.commands.registerCommand("vscode-samples-extension.find-files", async () => {
+		const globPattern = await vscode.window.showInputBox({
+			value: "**/*.js",
+		});
+		if (!globPattern) {
+			return;
+		}
+		const files = await vscode.workspace.findFiles(globPattern);
+		vscode.window.showInformationMessage(files ? "Result: " + files.join(" "): "Result: nothing");
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
